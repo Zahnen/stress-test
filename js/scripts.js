91c73aa780cc476792f4935b66a2be8a99d1eefc
Zahnen Garner <zahnen@gmail.com>
2020-10-27 15:41:09 -0700

    add suggestion functionality
    Co-authored-by: Austin Schrader <austin.jschrader@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,35 @@
 $(document).ready(function() {
   $("form#stressSurvey").submit(function(event) {
     event.preventDefault();
+    let badThings = [];
+    let goodThings = [];
 
 
-    userInput = $("input:checkbox[name=warningSigns]:checked").each(function() {
+    userInput1 = $("input:checkbox[name=warningSigns]:checked").each(function() {
       const warnOutput = $(this).val();
       $("#finalSymptoms").append("<li>" + warnOutput + "</li>")
+      badThings.push(warnOutput);
     });
+    userInput2 = $("input:checkbox[name=symptoms]:checked").each(function() {
+      const sympOutput = $(this).val();
+      $("#finalSymptoms").append("<li>" + sympOutput + "</li>")
+      badThings.push(sympOutput);
+    })
+    console.log("This is the new array" + badThings);
+    userInput3 = $("input:checkbox[name=coping]:checked").each(function() {
+      const copOutput = $(this).val();
+      $("#finalCoping").append("<li>" + copOutput + "</li>")
+      goodThings.push(copOutput);
+    })
 
+    $("#results").show();
 
-    $("#suggestions").show();
+    if (badThings.length > goodThings.length) {
+      $("#suggestions").show();
+    } else {
+      $("#noProblems").show();
+    }
+
+    
   });
 });
